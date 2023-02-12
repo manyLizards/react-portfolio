779239121a9792decd742f97610e7c4f97a326da
Jordan <jnbarringer96@gmail.com>
2023-02-11 19:34:38 -0600

    project data created

---
+++ public/src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+    {
+        title: "Accessibility Project",
+        subtitle: "HTML, CSS",
+        description:  "Given starter code, this project showcases the ability to make websites accessible",
+        image: "./project-1.png", //will add images later
+        link: "https://manylizards.github.io/Accessibility-Project/"
+    },
+    {
+        title: "Password Generator",
+        subtitle: "Javascript",
+        description:  "Given starter code, this project showcases the ability to use javascript to create functionality in a website.",
+        image: "./project-2.png", //will add images later
+        link: "https://manylizards.github.io/Password-Generator/"
+    },
+    {
+        title: "Rock Paper Scissors",
+        subtitle: "Javascript",
+        description:  "A game made from scratch",
+        image: "./project-3.png", //will add images later
+        link: "https://manylizards.github.io/rock-paper-scissors/"
+    },
+    {
+        title: "Weather Dashboard",
+        subtitle: "HTML, CSS, Javascript, OpenWeather API",
+        description:  "A website made from scratch to display the weather for a given city.",
+        image: "./project-4.png", //will add images later
+        link: "https://manylizards.github.io/Weather-Dashboard/"
+    },
+    {
+        title: "Everrands",
+        subtitle: "HTML, CSS, Javascript, nodemailer, Places API, Handlebars, Sequelize, Heroku",
+        description:  "A group project with the goal of connecting a community.",
+        image: "./project-5.png", //will add images later
+        link: "https://arcane-hollows-20210.herokuapp.com/"
+    },
+    {
+        title: "Law Office Website",
+        subtitle: "HTML, CSS, Javascript",
+        description:  "A group project for a mock law office website",
+        image: "./project-6.png", //will add images later
+        link: "https://manylizards.github.io/lawffice/index.html"
+    }
+]
