e10a96afef263ca8d39ef7d271da147afcb4b01e
Jordan <jnbarringer96@gmail.com>
2023-02-11 22:44:13 -0600

    added functionality to submit contact form

--- public/src/components/Footer.js
+++ public/src/components/Footer.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { Components } from "react";
 
 export default function Footer() {
+
+    //create dedicated state for each value from the form
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    //store what the user types in to each of the inputs
+    function encode(data) {
+        return Object.keys(data)
+        .map(
+            (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+    }
+
+    //handle the submission of the form to make a post request to the endpoint with all our form data
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", name, email, message }),
+        })
+        .then(() => alert("Message sent!"))
+        .catch((error) => alert(error));
+    }
+
     return (
         <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -66,6 +92,7 @@ export default function Footer() {
               id="name"
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -77,6 +104,7 @@ export default function Footer() {
               id="email"
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="relative mb-4">
@@ -89,6 +117,7 @@ export default function Footer() {
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={(e) => setMessage(e.target.value)}
             />
           </div>
           <button
