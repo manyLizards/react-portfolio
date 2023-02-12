bc6dc7dc04fd28b9c9b3ba511c4c7008f3c4a909
Jordan <jnbarringer96@gmail.com>
2023-02-11 18:57:42 -0600

    added more tailwind config

--- public/src/app.js
+++ public/src/app.js
@@ -3,13 +3,17 @@ import React from "react";
 //import sections
 import Header from "./components/Header";
 import Content from "./components/Content";
+import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+//import tailwind
+import "tailwindcss/tailwind.css";
 
 export default function App() {
     return (
         <main>
             <Header />
             <Content />
+            <Projects />
             <Footer />
         </main>
     );
