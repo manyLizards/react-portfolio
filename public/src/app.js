f3a5e25ad488b9465d42c181dd4b3764cdb21d7a
Jordan <jnbarringer96@gmail.com>
2023-02-11 18:39:09 -0600

    added files and imported components to app.js

--- public/src/app.js
+++ public/src/app.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+//import sections
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+
 export default function App() {
     return (
         <main>
